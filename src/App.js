0cafdd944777d9dbfb59d3e1ed2a5a8033f8b27c
Teslyak <124871929+Teslyak@users.noreply.github.com>
2024-02-06 13:20:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as yup from "yup";
+const initialValues = {
+  login: "",
+  password: "",
+};
+const userSchema = yup.object().shape({
+  login: yup.string().required(),
+  password: yup.string().min(6).max(16).required(),
+});
+export const handleSubmit = (value, { resetForm }) => {
+  console.log(value);
+  resetForm();
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={userSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <label htmlFor="login">Login</label>
+          <Field type="text" name="login" />
+          <ErrorMessage name="login" component="div" />
+          <label htmlFor="password">
+            Password
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" />
+          </label>
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
